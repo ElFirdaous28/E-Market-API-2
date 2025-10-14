5082465f60d419b2e69da554631873be719a3374
ayoubfetti <ayoubfetti.dev@gmail.com>
2025-10-14 15:15:49 +0100

    coupon routes

---
+++ routes/couponRoutes.js
@@ -0,0 +1,22 @@
+import express from "express";
+import * as couponController from "../controllers/couponController.js";
+import validate from "../middlewares/validate.js";
+import { couponSchema } from "../validations/couponSchema.js";
+import { isAuthenticated } from "../middlewares/auth.js";
+
+const router = express.Router();
+
+// // Toutes les routes nécessitent une authentification
+router.use(isAuthenticated);
+
+// Routes CRUD pour seller/admin (middleware seller/admin)
+router.post("/", validate(couponSchema), couponController.createCoupon);
+router.get("/", couponController.getAllCoupons);
+router.get("/:id", couponController.getCouponById);
+router.put("/:id", validate(couponSchema), couponController.updateCoupon);
+router.delete("/:id", couponController.deleteCoupon);
+
+// Route de validation pour utilisateurs authentifiés
+router.post("/validate", couponController.validateCoupon);
+
+export default router;
