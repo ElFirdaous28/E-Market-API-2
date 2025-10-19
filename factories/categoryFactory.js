3d9ce716dae38278d72ca94342cc8be1b1d231a7
ElFirdaous28 <elmokhtarifirdaous@gmqil.com>
2025-10-19 21:06:22 +0100

    reset db

--- factories/categoryFactory.js
+++ factories/categoryFactory.js
@@ -2,22 +2,24 @@ import Category from "../models/Category.js";
 import { faker } from "@faker-js/faker";
 
 /**
- * Creates one or more categories in the database.
+ * Creates one or more unique categories in the database.
  * @param {number} count - Number of categories to create (default 1)
  * @param {Object} overrides - Optional overrides applied to all categories
  * @returns {Promise<Category[]>} - Array of created category documents
  */
 export const categoryFactory = async (count = 1, overrides = {}) => {
-    const categories = [];
+    // Generate unique category names
+    const uniqueNames = faker.helpers.uniqueArray(faker.commerce.department, count);
 
-    for (let i = 0; i < count; i++) {
-        const category = await Category.create({
-            name: overrides.name || faker.commerce.department(), // random category name
-            ...overrides,
-        });
-
-        categories.push(category);
-    }
+    // Create all categories
+    const categories = await Promise.all(
+        uniqueNames.map(async (name) => {
+            return Category.create({
+                name: overrides.name || name,
+                ...overrides,
+            });
+        })
+    );
 
     return categories;
 };
