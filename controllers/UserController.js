e4116f6d502be72a6a576c9aed8ce4b6c7b7a146
samira kibous <kiboussamira71@gmail.com>
2025-10-28 20:49:18 +0100

    response fix

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -9,7 +9,12 @@ export const createUser = async (req, res, next) => {
     const user = new User(req.body);
     await user.save();
 
-    res.status(201).json({ message: "User created successfully", user });
+     res.status(201).json({
+      message: "User created successfully",
+      data: {
+        user
+      },
+    });
   } catch (error) {
     next(error);
   }
